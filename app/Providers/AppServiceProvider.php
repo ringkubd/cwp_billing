5c96035085a862fd2d429d0930f53c1ba67c8a03
anwar jahid <ajr.jahid@gmail.com>
2020-04-13 14:13:11 +0600

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * Class AppServiceProvider.
@@ -33,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+		Schema::defaultStringLength(191);
         /*
          * Application locale defaults for various components
          *
